d5f0b7790b83d2cc252c3b857dd37000802e4717
princeShozanah <max.pechko@gmail.com>
2017-12-25 18:33:43 +0200

    0.0.1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBH7e8MlyFtghJgDqPawkZlAJtxu_zHYvs",
+    authDomain: "contructor-7553e.firebaseapp.com",
+    databaseURL: "https://contructor-7553e.firebaseio.com",
+    projectId: "contructor-7553e",
+    storageBucket: "contructor-7553e.appspot.com",
+    messagingSenderId: "971261132352"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
